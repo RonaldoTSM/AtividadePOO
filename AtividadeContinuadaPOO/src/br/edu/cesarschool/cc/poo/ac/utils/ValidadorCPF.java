742139e2a8a8d2ee978b35f8a1b1e7bf1a1061d1
RonaldoTSM <ronaldotsm@outlook.com>
2024-04-19 22:07:07 -0300

    Stage 1 - Specification 1/4

--- AtividadeContinuadaPOO/src/br/edu/cesarschool/cc/poo/ac/utils/ValidadorCPF.java
+++ AtividadeContinuadaPOO/src/br/edu/cesarschool/cc/poo/ac/utils/ValidadorCPF.java
@@ -4,8 +4,29 @@ public class ValidadorCPF {
 	
 	private ValidadorCPF() {		
 	}
-	
-	//public static boolean isCpfValido(String cpf) {				
-	//}
+
+	public static boolean isCpfValido(String cpf) {
+		
+		if (cpf == null || cpf.length() != 11 || !cpf.matches("[0-9]+")) {
+			return false;
+		}
+
+		int soma = 0, resto;
+		for (int i = 0; i < 9; i++) {
+			soma += Integer.parseInt(cpf.substring(i, i + 1)) * (10 - i);
+		}
+		resto = 11 - (soma % 11);
+		int primeiroDigitoVerificador = (resto > 9) ? 0 : resto;
+
+		soma = 0;
+		for (int i = 0; i < 10; i++) {
+			soma += Integer.parseInt(cpf.substring(i, i + 1)) * (11 - i);
+		}
+		resto = 11 - (soma % 11);
+		int segundoDigitoVerificador = (resto > 9) ? 0 : resto;
+		
+		return primeiroDigitoVerificador == Character.getNumericValue(cpf.charAt(9)) &&
+				segundoDigitoVerificador == Character.getNumericValue(cpf.charAt(10));
+	}
 
 }
